05783677b3f427fa7f7e848cd7717c758166e364
Aryan Bola <aaryann5002@gmail.com>
2025-04-28 01:08:18 +0530

    one step closer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
 
@@ -9,13 +11,18 @@ app.use(cors({
   credentials:true 
 }))
 
+//we are limiting the data that can be taken by url,json,etc
+// Express middleware to parse JSON bodies sent by frontend.	To prevent very large payloads from attacking your server (DDoS).
 app.use(express.json({ limit: " 16kb" }))
+//Express middleware to parse URL-encoded bodies (like data submitted by HTML forms). | extended: true: ->	Allows parsing of nested objects inside URL-encoded forms.
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+//Serves static files like images, CSS, JavaScript from a public/ folder
 app.use(express.static('public'))
 app.use(cookieParser())
 
 import userRouter from './routes/user.routes.js'
 
-app.use('/api/v1.users',userRouter)
+app.use('/api/v1/users',userRouter)
+console.log("User routes registered at /api/v1/users");
 
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,9 @@
+//if there was some problem in any async function the backend will crash so we use the try catch method but insead of writing it everywhere we just use this function which just catches the error
+
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
-      Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    Promise.resolve(requestHandler(req, res, next))
+      .catch((err) => next(err)) // sends error to the error handling middleware of express
   }
 }
 
