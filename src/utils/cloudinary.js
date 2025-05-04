668a1c65894d83bd208a3ec0b2f0245e132e6775
Aryan Bola <aaryann5002@gmail.com>
2025-05-04 16:53:20 +0530

    added pipeline

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -26,6 +26,21 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
+const deleteFromCloudinary = async (cloudUrl) => {
+    try {
+        // Extract the public ID from the URL
+        const publicId = cloudUrl.split("/").pop().split(".")[0];
+
+        // Delete the file using the public ID
+        const result = await cloudinary.uploader.destroy(publicId);
+        console.log("Deleted:", result);
+    
+        return result;
+        } catch (error) {
+        console.error("Error deleting file:", error);
+        }
+    };
 
 
-export {uploadOnCloudinary}
+
+export {uploadOnCloudinary,deleteFromCloudinary}
