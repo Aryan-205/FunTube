c949e6734aa2673d809d765f0a8cacfe0e143714
Aryan Bola <aaryann5002@gmail.com>
2025-05-22 14:42:15 +0530

    added video like feature

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,6 +7,46 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+
+    if(!videoId){
+        throw new ApiError(400, "videoId not foundj")
+    }
+
+    const userId = req.user.id
+
+    if (!videoId || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Missing or Invalid video id");
+    }
+
+    const existingLike =  await Like.findById({
+        video:videoId,
+        likedBy:userId
+    })
+    let liked;
+
+    if(existingLike){
+        const deletedVideoLike =  await existingLike.deleteOne()
+
+        if (!deletedVideoLike) {
+            throw new ApiError(500, "Failed to unlike the video");
+        }
+        liked = false;
+    } else {
+        const likedVideo =  await Like.create({
+            video:videoId,
+            likedBy:userId
+        })
+        if(likedVideo){
+            throw new ApiError(400, "Failed to like the video")
+        }
+        liked = true;
+    }
+
+    const totalLike =  await Like.countDocuments({video: videoId})
+
+    return res.status(200).json(
+        new ApiResponse(200,{ videoId, liked, totalLike },"video liked successfully")
+    )
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
